41a4154cc0fcf2bd3aeefce3eeb3f50b93f25dae
Alem Čaušević <ac2590@student.uni-lj.si>
2024-03-15 20:53:22 +0100

    project files

---
+++ src/test/java/com/app/flightnightservices/FlightNightServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.flightnightservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightNightServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
